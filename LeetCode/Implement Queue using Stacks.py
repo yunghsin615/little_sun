55f4b8ecd6f57552e70a087f952b8df1996edeae
yunghsin615 <55618445+yunghsin615@users.noreply.github.com>
2019-12-31 20:56:31 +0800

    Create Implement Queue using Stacks.py

---
+++ LeetCode/Implement Queue using Stacks.py
@@ -0,0 +1,50 @@
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.inputStack = []
+        self.resStack = []
+        
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        """
+        self.inputStack.append(x)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        if self.inputStack:
+            a = self.inputStack[0]
+            self.inputStack.pop(0)
+            return a
+        
+        else:
+            return
+  
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        if self.inputStack:
+            return self.inputStack[0]
+        
+        else:
+            return
+   
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        if len(self.inputStack) == 0:
+            return True
+        return False
