0df10d27da63586badc433b1391d11af9e6889fd
yunghsin615 <55618445+yunghsin615@users.noreply.github.com>
2020-01-01 14:13:18 +0800

    Update MinStack.py

--- LeetCode/MinStack.py
+++ LeetCode/MinStack.py
@@ -6,12 +6,11 @@ class Node:
 class MinStack(object):
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
+        
         self.arr = []
         
     def push(self, x):
+        
         if len(self.arr) == 0:
             self.arr.append(x)
             self.temp_min = x
@@ -20,23 +19,16 @@ class MinStack(object):
             if self.temp_min > self.arr[-1]:
                 self.temp_min = self.arr[-1]
 
-
     def pop(self):
-        """
-        :rtype: None
-        """
+  
         self.arr.pop()
 
     def top(self):
-        """
-        :rtype: int
-        """
+        
         return self.arr[-1]
 
     def getMin(self):
-        """
-        :rtype: int
-        """
+        
         sort = sorted(self.arr)
         return sort[0]
 
